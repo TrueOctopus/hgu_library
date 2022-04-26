78ce369582b6dcd914ebcd9ce65864e4da11e929
TrueOctopus <18368876370@163.com>
2022-04-26 17:40:24 +0800

    修改留言校验

--- src/views/CommentHub/Comment.vue
+++ src/views/CommentHub/Comment.vue
@@ -1,22 +1,24 @@
 <!--
  * @Author: 郑钊宇
  * @Date: 2022-03-25 16:01:50
- * @LastEditTime: 2022-04-24 19:50:41
+ * @LastEditTime: 2022-04-26 17:39:08
  * @LastEditors: 郑钊宇
  * @Description:
 -->
 <template>
   <div class="md-layout">
 
-    <md-field class="md-layout-item md-size-70">
-      <label style="font-size: 16px">留言区</label>
-      <md-textarea v-model="submitForm.messagecontent" :disabled="isLogin" />
+    <md-field class="md-layout-item md-size-70" :class="commentMessageClass">
+      <label class="label">留言区</label>
+      <span class="md-error">{{ commentErrorMessage }}</span>
+      <md-textarea v-model="submitForm.messagecontent" :disabled="isLogin" @blur="commentVerify" />
+
     </md-field>
     <div class="submitBtn md-layout-item md-size-20">
       <md-button class="md-info" @click="submit">留言</md-button>
     </div>
 
-    <div class="md-layout-item md-size-100">
+    <div class="md-layout-item md-size-100 list">
       <commentElement
         v-for="item in list"
         :key="item.id"
@@ -57,6 +59,15 @@ export default {
         messagecontent: '',
         commenter: '',
         checkflag: 0
+      },
+      hasCommentErrorMessage: false,
+      commentErrorMessage: ''
+    }
+  },
+  computed: {
+    commentMessageClass() {
+      return {
+        'md-invalid': this.hasCommentErrorMessage
       }
     }
   },
@@ -83,14 +94,30 @@ export default {
         // console.log(document.getElementsByClassName('wrapper')[0])
       })
     },
+    commentVerify() {
+      if (this.submitForm.messagecontent.length < 1) {
+        this.hasCommentErrorMessage = true
+        this.commentErrorMessage = '请输入留言内容'
+      } else if (this.submitForm.messagecontent.length < 16) {
+        this.hasCommentErrorMessage = true
+        this.commentErrorMessage = '留言内容不能少于15个字'
+      } else {
+        this.hasCommentErrorMessage = false
+        this.commentErrorMessage = ''
+      }
+    },
     submit() {
-      addComment(this.submitForm).then(res => {
+      this.commentVerify()
+      if (!this.hasCommentErrorMessage) {
+        addComment(this.submitForm).then(res => {
         // console.log(res)
-        this.$store.dispatch('notification/promptNotification', {
-          noticeType: 'success',
-          message: '留言成功,请记得查收'
+          this.$store.dispatch('notification/promptNotification', {
+            noticeType: 'success',
+            message: '留言成功,请记得查收哦'
+          })
+          this.submitForm.messagecontent = ''
         })
-      })
+      }
     }
   }
 }
@@ -99,5 +126,16 @@ export default {
 <style lang="scss" scoped>
 .submitBtn {
   margin-top: 7vh;
+}
+.label {
+  font-size: 16px;
+  top: -1vh !important;
+}
+.list {
+  margin-top: 2vh;
+}
+.md-error {
+  margin-top: -6.4vh;
+  margin-left: 60px;
 }
 </style>
